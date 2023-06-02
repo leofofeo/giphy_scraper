8e735e8fa17fb6e338b0ddf83b09362be61662eb
leorubiano <leo>
2023-06-02 16:37:10 -0400

    replace get_in with direct access, fix api_key variable name

--- lib/giphy_scraper/fetcher.ex
+++ lib/giphy_scraper/fetcher.ex
@@ -1,7 +1,7 @@
 defmodule GiphyScraper.Fetcher do
   alias GiphyScraper.GiphyImage
 
-  def get_gifs_for_query(query, limit \\ 25) do
+  def get_gifs_for_query(query, limit \\ 2) do
     query
     |> get_giphy_request_url(limit)
     |> make_request_and_return_response_data
@@ -9,22 +9,22 @@ defmodule GiphyScraper.Fetcher do
   end
 
   def get_giphy_request_url(query, limit) do
-    api_key = get_api_key()
     "api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{query}&limit=#{limit}"
   end
 
   def make_request_and_return_response_data(url) do
     HTTPoison.start
     {:ok, response} = HTTPoison.get(url)
+    IO.inspect response
     body = response.body |> JSON.decode!
     body["data"]
   end
 
   def parse_response_data_into_image_data(data) do
-    title = get_in(data, ["title"])
-    url = get_in(data, ["url"])
-    username = get_in(data, ["user", "username"])
-    id = get_in(data, ["id"])
+    title = data["title"]
+    url = data["url"]
+    username = data["user"]["username"]
+    id = data["id"]
     %GiphyImage{
       title: title,
       url: url,
@@ -33,5 +33,5 @@ defmodule GiphyScraper.Fetcher do
     }
   end
 
-  defp get_api_key, do: System.get_env("GIPHY_API_KEY")
+  defp api_key, do: System.get_env("GIPHY_API_KEY")
 end
