ac74b416c31c316b8bce5774019c8f73e3b2a502
leorubiano <leo>
2023-06-02 18:31:57 -0400

    update error handling for poison.get

--- lib/giphy_scraper/fetcher.ex
+++ lib/giphy_scraper/fetcher.ex
@@ -1,7 +1,7 @@
 defmodule GiphyScraper.Fetcher do
   alias GiphyScraper.GiphyImage
 
-  def get_gifs_for_query(query, limit \\ 2) do
+  def get_gifs_for_query(query, limit \\ 25) do
     query
     |> get_giphy_request_url(limit)
     |> make_request_and_return_response_data
@@ -9,15 +9,17 @@ defmodule GiphyScraper.Fetcher do
   end
 
   def get_giphy_request_url(query, limit) do
-    "api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{query}&limit=#{limit}"
+    "api.giphy.com/v1/gifs/search?api_key=#{api_key()}&q=#{query}&limit=#{limit}"
   end
 
   def make_request_and_return_response_data(url) do
     HTTPoison.start
-    {:ok, response} = HTTPoison.get(url)
-    IO.inspect response
-    body = response.body |> JSON.decode!
-    body["data"]
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}}
+        -> body |> JSON.decode! |> Kernel.get_in(["data"])
+      {:ok, %HTTPoison.Response{status_code: _}} -> %{}
+      {:error, %HTTPoison.Error{reason: _}} -> %{}
+    end
   end
 
   def parse_response_data_into_image_data(data) do
